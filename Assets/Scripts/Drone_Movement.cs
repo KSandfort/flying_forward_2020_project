3b99f170943e8e695cc929d96d4d6c79fd7ed36f
k-san-official <konstantin.sandfort@gmail.com>
2022-11-16 12:05:56 +0100

    Added some controls

---
+++ Assets/Scripts/Drone_Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class Drone_Movement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float _speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnLeftStick(InputValue value) {
+        Vector2 input_vec = value.Get<Vector2>();
+        Debug.Log(input_vec);
+        Vector3 target_dir = new Vector3(0, input_vec[1], 0);
+        _rigidbody.velocity = target_dir * _speed;
+    }
+
+    private void OnRightStick(InputValue value) {
+        Vector2 input_vec = value.Get<Vector2>();
+        Debug.Log(input_vec);
+        Vector3 target_dir = new Vector3(input_vec[0], 0, input_vec[1]);
+        _rigidbody.velocity = target_dir * _speed;
+    }
+}
