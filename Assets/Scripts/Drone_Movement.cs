103d380bf366a5271e6bbc375e8dac5432cc5765
k-san-official <konstantin.sandfort@gmail.com>
2022-11-23 11:33:56 +0100

    added rotation controls (not final)

--- Assets/Scripts/Drone_Movement.cs
+++ Assets/Scripts/Drone_Movement.cs
@@ -26,6 +26,9 @@ public class Drone_Movement : MonoBehaviour
         Debug.Log(input_vec);
         Vector3 target_dir = new Vector3(0, input_vec[1], 0);
         _rigidbody.velocity = target_dir * _speed;
+        Vector3 m_EulerAngleVelocity = new Vector3(0, input_vec[0] * 20, 0);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+        _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
     }
 
     private void OnRightStick(InputValue value) {
