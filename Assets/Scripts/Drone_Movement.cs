ff8f491ba126efcf8d963f64a4c3182c692e1380
k-san-official <konstantin.sandfort@gmail.com>
2022-12-07 11:49:39 +0100

    Fixed drone tilt on input

--- Assets/Scripts/Drone_Movement.cs
+++ Assets/Scripts/Drone_Movement.cs
@@ -15,29 +15,32 @@ public class Drone_Movement : MonoBehaviour
     Vector3 left_stick_vec = new Vector3(0, 0, 0);
     Vector3 right_stick_vec = new Vector3(0, 0, 0);
     Vector3 rotation_vel_vec = new Vector3(0, 0, 0);
+    GameObject tilt_controller;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        tilt_controller = GameObject.Find("Tilt_Controller");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {   
+        // Combine both stick inputs for target direction (xyz)
         target_dir = (left_stick_vec + right_stick_vec); // Combine left and right stick inputs
-        // Rotate drone
-        Vector3 x_z_rotation = new Vector3(right_stick_vec[2] * max_tilt, 0,-right_stick_vec[0] * max_tilt); // TODO: Find current y rotation
+        
+        // Rotate drone (tilt)
+        Vector3 x_z_rotation = new Vector3(right_stick_vec[2] * max_tilt, 0, -right_stick_vec[0] * max_tilt); // TODO: Find current y rotation
         Quaternion x_z_quaternion = Quaternion.Euler(x_z_rotation);
-        gameObject.transform.Find("drone_model").rotation = x_z_quaternion;
-
+        tilt_controller.transform.localRotation = x_z_quaternion;
+        
+        // Rotate drone (yaw)
         Quaternion deltaRotation = Quaternion.Euler(rotation_vel_vec);
         _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+        
         // Add velocity
         _rigidbody.velocity += (_rigidbody.rotation * target_dir);
-
-        // Quaternion x_z_deltaRotation = Quaternion.Euler(x_z_rotation * max_tilt);
-        //_rigidbody.MoveRotation(x_z_deltaRotation);
     }
 
     // Called when left stick input is perceived.
